d33ec1c688f42f1e0c8013423a6944ca9be110af
Anders Evenrud <andersevenrud@gmail.com>
2022-03-17 02:01:30 +0100

    ci(xpra): add analyzer plugin support

--- packages/xpra/vite.config.ts
+++ packages/xpra/vite.config.ts
@@ -1,10 +1,17 @@
 import * as path from 'path'
 import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const plugins = []
+
+if (process.env.VITE_STATS) {
+  plugins.push(visualizer())
+}
 
 export default defineConfig({
   base: '',
-  plugins: [dts()],
+  plugins: [dts(), ...plugins],
   build: {
     sourcemap: true,
     lib: {
