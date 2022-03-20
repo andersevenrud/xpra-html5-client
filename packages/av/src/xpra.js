5862dda2cddc7bd4cf40a159a47e7fc8645a26cb
Anders Evenrud <andersevenrud@gmail.com>
2022-03-20 03:41:47 +0100

    refactor(av): modernize xpra integration

--- packages/av/src/xpra.js
+++ packages/av/src/xpra.js
@@ -2,48 +2,31 @@
 // MIT licensed
 import AV from './av'
 
-(function() {
-  var __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+AV.XpraSource = class XpraSource extends AV.EventEmitter {
+  start() {
+    return true
+  }
 
-  AV.XpraSource = (function(_super) {
-    __extends(XpraSource, _super);
+  pause() {
+    return true
+  }
 
-    function XpraSource() {
-      // constructor
-    }
+  reset() {
+    return true
+  }
 
-    XpraSource.prototype.start = function() {
-      return true;
-    };
+  _on_data(data) {
+    var buf = new AV.Buffer(data);
+    return this.emit('data', buf);
+  }
+}
 
-    XpraSource.prototype.pause = function() {
-      return true;
-    };
+AV.Asset.fromXpraSource = function() {
+  var source = new AV.XpraSource();
+  return new AV.Asset(source);
+};
 
-    XpraSource.prototype.reset = function() {
-      return true;
-    };
-
-    XpraSource.prototype._on_data = function(data) {
-      var buf = new AV.Buffer(data);
-      return this.emit('data', buf);
-    };
-
-    return XpraSource;
-
-  })(AV.EventEmitter);
-
-  AV.Asset.fromXpraSource = function() {
-    var source;
-    source = new AV.XpraSource();
-    return new AV.Asset(source);
-  };
-
-  AV.Player.fromXpraSource = function() {
-    var asset;
-    asset = AV.Asset.fromXpraSource();
-    return new AV.Player(asset);
-  };
-
-}).call(this);
+AV.Player.fromXpraSource = function() {
+  var asset = AV.Asset.fromXpraSource();
+  return new AV.Player(asset);
+};
