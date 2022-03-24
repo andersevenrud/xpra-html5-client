cca7c32fb4109fd91c9314ba140ffeadfc89d1d2
Anders Evenrud <andersevenrud@gmail.com>
2022-03-25 00:36:02 +0100

    chore(xpra): move jest config to package.json

--- packages/xpra/jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  testMatch: ['**/tests/**/*.ts'],
-  preset: 'ts-jest/presets/js-with-ts-esm',
-  testEnvironment: 'jsdom',
-}
