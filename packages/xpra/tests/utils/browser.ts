f2664b68ce9833cef6b8a1df8fe3222c631f76b6
Anders Evenrud <andersevenrud@gmail.com>
2022-03-25 01:00:11 +0100

    test(utils): add preliminary browser utils units

---
+++ packages/xpra/tests/utils/browser.ts
@@ -0,0 +1,32 @@
+import { parseUrlQuerySearch } from '../../src/utils/browser'
+
+describe('Browser utilities', () => {
+  describe('parseUrlQuerySearch', () => {
+    const search =
+      '?number=1&booleanString=on&booleanNumber=1&booleanBoolean=true&list=a,b,c'
+
+    const options = {
+      numbers: ['number'],
+      booleans: ['booleanString', 'booleanNumber', 'booleanBoolean'],
+      lists: ['list'],
+    }
+
+    it('should return all values', () => {
+      expect(parseUrlQuerySearch(search, options)).toEqual({
+        number: 1,
+        booleanString: true,
+        booleanNumber: true,
+        booleanBoolean: true,
+        list: ['a', 'b', 'c'],
+      })
+    })
+
+    it('should return required values', () => {
+      expect(
+        parseUrlQuerySearch(search, { ...options, required: ['number'] })
+      ).toEqual({
+        number: 1,
+      })
+    })
+  })
+})
